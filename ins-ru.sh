2d7e1fb0263a4201c81ad7bac1be643dfcc10d88
mazeramcom <55714857+mazeramcom@users.noreply.github.com>
2021-02-27 17:24:16 +0300

    Add files via upload

---
+++ ins-ru.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Выберите, что вы хотите сделать:"
+echo "1) Установка"
+echo "2) Удаление скрипта"
+echo "3) Редактирование"
+read x
+if test $x -eq 1; then
+echo
+echo "Создание конфигурации сервера"
+wget -O /etc/profile.d/sshinfo.sh https://raw.githubusercontent.com/mazeram/motd/main/mazeram-info.sh
+echo "Скрипт установлен"
+ 
+# runs this if option 2 is selected
+elif test $x -eq 2; then
+rm /etc/profile.d/sshinfo.sh
+echo "Скрипт удалён"
+# runs this if option 3 is selected
+elif test $x -eq 3; then
+nano /etc/profile.d/sshinfo.ssh
+exit
+fi
