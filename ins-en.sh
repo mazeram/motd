d47032c2b14c310bcddd664a06c852d99d60d76a
mazeramcom <55714857+mazeramcom@users.noreply.github.com>
2021-02-27 20:38:22 +0300

    Create ins-en.sh

---
+++ ins-en.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Choose what you want to do:"
+echo "1) Install motd"
+echo "2) Remove motd"
+echo "3) Edit"
+read x
+if test $x -eq 1; then
+echo
+echo "Creating a motd file"
+sudo  wget -O /etc/profile.d/sshinfo.sh https://raw.githubusercontent.com/mazeram/motd/main/mazeram-info.sh
+echo "Motd installed"
+ 
+# runs this if option 2 is selected
+elif test $x -eq 2; then
+sudo  rm /etc/profile.d/sshinfo.sh
+echo "Motd removed"
+# runs this if option 3 is selected
+elif test $x -eq 3; then
+sudo  nano /etc/profile.d/sshinfo.sh
+exit
+fi
